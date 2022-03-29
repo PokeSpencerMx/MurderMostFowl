b0565e6e6a00982815731ac6ed67adf44780495f
PokeSpencerMx <91617455+PokeSpencerMx@users.noreply.github.com>
2022-03-29 16:33:45 -0400

    Initial commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    public string Name;
+    public string Description;
+    public GameObject TextBox;
+    public GameObject UIText;
+    public GameObject Player;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text InteractedWith = UIText.GetComponent<Text>();
+        InteractedWith.text = Description;
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void OnInteract()
+    {
+        
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        TextBox.SetActive(true);
+    }
+
+    public virtual void OnTriggerExit2D(Collider2D collision)
+    {
+        TextBox.SetActive(false);
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private SpriteRenderer sprRend;
+    [SerializeField] private bool JustSat = false;
+
+    private float speedModifier;
+
+    //This next public pool will be used in the Interactable Script.
+    [SerializeField] public bool FreezeMovement;
+    
+    void Update()
+    {
+        if(FreezeMovement == true)
+        {
+            speedModifier = 0f;
+        } else
+        {
+            speedModifier = 3f;
+        }
+        
+        
+        float walkSpeedLeftRight = Input.GetAxisRaw("Horizontal") * speedModifier;
+        float walkSpeedUpDown = Input.GetAxisRaw("Vertical") * speedModifier;
+
+        transform.position += new Vector3(walkSpeedLeftRight * Time.deltaTime, walkSpeedUpDown * Time.deltaTime, 0);
+
+        if(walkSpeedLeftRight == 0 && walkSpeedUpDown == 0)
+        {
+            Idle();
+        }
+        else
+        {
+            NotIdle(walkSpeedLeftRight);
+        }
+    }
+
+    private void Idle()
+    {
+        if(JustSat == false)
+        {
+            playerAnimator.Play("Deteggtive-Idle");
+            JustSat = true;
+        }
+        else
+        {
+            playerAnimator.Play("Deteggtive-Sit");
+        }
+        //In this code I'm trying to make sure the idle animation in which the player sits
+        //Happens once, then the Sit animation continues.
+        
+        
+    }
+
+    private void NotIdle(float move)
+    {
+        JustSat = false;
+        playerAnimator.Play("Deteggtive-Walk");
+
+        if(move > 0)
+        {
+            sprRend.flipX = false;
+        } 
+        else if (move < 0) 
+        {
+            sprRend.flipX = true;
+        }
+    }
+}
